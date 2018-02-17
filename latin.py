e2c9e88a4e291b363d2852da096b41959111b669
pk061999 <panoskinnas@yahoo.com>
2018-02-17 13:34:47 +0200

    latin.py

---
+++ latin.py
@@ -0,0 +1,20 @@
+num = int(input("Enter a number: "))
+
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+
+def num2roman(num):
+
+    roman = ''
+
+    while (num > 0 and num<=1000000):
+        for i, r in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+
+    return roman
+
+  
+print(num2roman(num))
