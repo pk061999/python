59a4cb6f6b6737562536ba7c34113542d5d0dc96
pk061999 <36563624+pk061999@users.noreply.github.com>
2018-02-21 19:32:25 +0200

    Update latin.py

--- latin.py
+++ latin.py
@@ -1,4 +1,4 @@
-num = int(input("Enter a number: "))
+num = int(input("balte enan arithmo: "))
 
 num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
